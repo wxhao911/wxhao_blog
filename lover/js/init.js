9453f5c116a50fba1aa4a2e222364809d3973b1e
627760565@qq.com <627760565@qq.com>
2016-05-20 17:22:05 +0800

    20160520 love update

--- lover/js/init.js
+++ lover/js/init.js
@@ -1,16 +1,1 @@
-// 初始化
-if(typeof Cookies.get('search_engine') === "undefined")
-	Cookies.set('search_engine', 'baidu', {expires: 365, path: '/'});
 
-// 默认引擎
-$('#search_engine').val(Cookies.get('search_engine'));
-switch($('#search_engine').val()) {
-	case 'baidu':
-		$('#search_bar').attr('action', 'http://www.baidu.com/s');
-		$('#search').attr('name', 'wd');
-		break;
-	case 'google':
-		$('#search_bar').attr('action', 'https://www.google.com/search');
-		$('#search').attr('name', 'q');
-		break;
-}
